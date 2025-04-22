fc43f36b383c840cde7f85037e090755f97232e7
Ruta-007 <francis.rutatina@stud.umu.ac.ug>
2025-04-22 20:12:14 +0000

    Updated scripts

--- backup_script.sh
+++ backup_script.sh
@@ -1,7 +1,8 @@
-#!'/bin/bash
-SOURCE_DIR="home/Tatney/university/StudentRecords"
-BACKUP_DIR="home/Tatney/BACKUP_DIR"
-DATE=$(date +%Y-%m-%d")
-tar -czf "$BACKUP_DIR/StudentRecords_BACKUP_DIR_$DATE.tar.gz" "SOURCE_DIR"
-echo "$(date +"Y-%m-%d %H:%M:%S")
-- Backup created at $BACKUP_DIR/StudentRecords_BACKUP_DIR_$DATE.tar.gz" >> /var/log/backup.log
+#!/bin/bash
+SOURCE_DIR="/home/Tatney/university/StudentRecords"
+BACKUP_DIR="/home/Tatney/BACKUP_DIR"
+DATE=$(date +%Y-%m-%d)
+LOG_FILE="/home/Tatney/backup.log"
+mkdir -p "$BACKUP_DIR"
+tar -czf "$BACKUP_DIR/StudentRecords_BACKUP_$DATE.tar.gz" "$SOURCE_DIR"
+echo "$(date +'Y-%m-%d %H:%M:%S') - Backup created at $BACKUP_DIR/StudentRecords_BACKUP_$DATE.tar.gz" >> "$LOG_FILE"
